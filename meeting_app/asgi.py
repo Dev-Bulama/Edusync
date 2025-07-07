cc5d183a14e76ccfa0977a504505d959abfb74fa
Dev-Bulama <bulamabukar10906@gmail.com>
2025-07-07 10:17:32 +0100

    updated

--- meeting_app/asgi.py
+++ meeting_app/asgi.py
@@ -1,24 +1,28 @@
+"""
+ASGI config for meeting_app project.
+It exposes the ASGI callable as a module-level variable named ``application``.
+"""
+
 import os
-import django
 from django.core.asgi import get_asgi_application
-
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meeting_app.settings')
-django.setup()
-
-# Import after Django setup
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
-# Import routing after Django is set up
+# Import your routing
 import core.routing
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meeting_app.settings')
+
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
     "http": django_asgi_app,
+
+    # WebSocket chat handler
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
